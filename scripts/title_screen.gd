1786502f41c7936986e0042ab055bebe7418ac4f
Gustavo Machado <gustavomachado.521@gmail.com>
2026-03-03 16:42:14 -0300

    feat: button config

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,22 +1,26 @@
 extends Control
 
-#buttons
-@onready var button_play = $ButtonPlay
-@onready var button_exit = $ButtonExit
+#popup
+@onready var PopUp = preload("res://scenes/pop_up.tscn")
 
 #labels
-@onready var button_play_label = $ButtonPlay/TextureButton/Label
-@onready var button_exit_label = $ButtonExit/TextureButton/Label
+@onready var button_play_label = $VBoxContainer/PlayButton/TextureButton/Label
+@onready var button_exit_label = $VBoxContainer/ExitButton/TextureButton/Label
 
 func _ready():
 	#insert text in buttons
 	button_play_label.text = "JOGAR"
 	button_exit_label.text = "SAIR"
 
-func _on_button_play_pressed() -> void:
+func _on_play_button_pressed() -> void:
 	print("Botão JOGAR clicado!")
 	get_tree().change_scene_to_file("res://scenes/home.tscn")
 
-func _on_button_exit_pressed() -> void:
+func _on_exit_button_pressed() -> void:
 	print("Botão SAIR clicado!")
 	get_tree().quit()
+
+
+func _on_config_button_pressed() -> void:
+	var pop_up = PopUp.instantiate()
+	add_child(pop_up)
