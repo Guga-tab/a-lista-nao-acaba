3f32342eebe1e9a94e384a7e54c8c34f3f5bcef8
Gustavo Carlos <gusna.04@outlook.com>
2026-03-05 22:03:16 -0300

    resolvido crash ao apertar o botão de config na tela inicial

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -27,6 +27,6 @@ func _on_exit_button_pressed() -> void:
 
 func _on_config_button_pressed() -> void:
 	click_sound.play()
-	await click_sound.finish()
+	await click_sound.finished
 	var pop_up = PopUp.instantiate()
 	add_child(pop_up)
