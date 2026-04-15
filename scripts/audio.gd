a987ee1b640b424f6b339ec9d7aeeb5bdc910d1f
Gustavo Carlos <gusna.04@outlook.com>
2026-04-15 17:07:58 -0300

    feat: adicionado pomodoro e slider de volume

--- scripts/audio.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-var master_bus_index = AudioServer.get_bus_index("Master")
-
-func _on_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
