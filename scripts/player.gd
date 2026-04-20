4fd58b428f18839973ccd77b5d9a495a8a39134b
Gustavo Carlos <gusna.04@outlook.com>
2026-04-20 15:55:17 -0300

    feat: adicionado personagem princiap

---
+++ scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed: float = 250.0
+
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+
+	# Movimento horizontal
+	direction.x = Input.get_axis("ui_left", "ui_right")
+
+	# Movimento vertical
+	direction.y = Input.get_axis("ui_up", "ui_down")
+
+	# Normaliza diagonal
+	direction = direction.normalized()
+
+	velocity = direction * speed
+
+	move_and_slide()
