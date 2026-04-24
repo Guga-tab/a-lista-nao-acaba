29484fcd8fecac07c5eb38614d92b21bd79077ea
Gustavo Carlos <gusna.04@outlook.com>
2026-04-23 23:52:37 -0300

    feat: refeita tela de cosméticos

--- scripts/user/user_screen.gd
+++ scripts/user/user_screen.gd
@@ -1,12 +1,12 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/home.tscn")
+	get_tree().change_scene_to_file("res://scenes/screens/home.tscn")
 
 
 func _on_shop_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/store.tscn")
+	get_tree().change_scene_to_file("res://scenes/screens/store.tscn")
 
 
 func _on_skin_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/inventory.tscn")
+	get_tree().change_scene_to_file("res://scenes/screens/inventory.tscn")
